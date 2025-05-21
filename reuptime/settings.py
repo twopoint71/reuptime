3806fd65d3886e26f4b8d4c0e5990b2398e8fae7
Bob Smith <twopoint71@gmail.com>
2025-05-21 00:26:23 -0500

    Take another shot at getting static dir correct

--- reuptime/settings.py
+++ reuptime/settings.py
@@ -46,9 +46,6 @@ INSTALLED_APPS = [
 # Static files configuration
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles'),
-]
 
 # Only use WhiteNoise in production
 if not DEBUG:
